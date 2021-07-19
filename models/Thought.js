9edc95f27bea7061853a586a8efefbea6c16178c
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-07-19 09:51:01 -0500

    added user routes and controllers

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,6 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+const User = require('./User');
 
 const ReactionSchema = new Schema(
     {
@@ -62,5 +63,35 @@ ThoughtSchema.virtual('reactionCount').get(function () {
 });
 
 const Thought = model('Thought', ThoughtSchema);
+
+ReactionSchema.post("remove", document => {
+    const reactionId = document._id;
+    Thought.find({ reactions: { $in: [reactionId] } }).then(users => {
+        Promise.all(
+            users.map(user =>
+                User.findOneAndUpdate(
+                    user._id,
+                    { $pull: { reactions: reactionId } },
+                    { new: true }
+                )
+            )
+        );
+    });
+});
+
+ThoughtSchema.post("remove", document => {
+    const thoughtId = document._id;
+    User.find({ thoughts: { $in: [thoughtId] } }).then(users => {
+        Promise.all(
+            users.map(user =>
+                User.findOneAndUpdate(
+                    user._id,
+                    { $pull: { thoughts: thoughtId } },
+                    { new: true }
+                )
+            )
+        );
+    });
+});
 
 module.exports = Thought;
